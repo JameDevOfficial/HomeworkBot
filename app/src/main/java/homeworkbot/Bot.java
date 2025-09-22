cf6793fd7278c7c69aac3db4652851a408856950
jamedevofficial <jame.bayerl@gmai.com>
2025-09-22 18:33:30 +0200

    added logging for discord username

--- app/src/main/java/homeworkbot/Bot.java
+++ app/src/main/java/homeworkbot/Bot.java
@@ -2,15 +2,24 @@ package homeworkbot;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
 import reactor.core.publisher.Mono;
 
 public class Bot {
 
     public static void main(String[] args) {
         String token = System.getenv("TOKEN");
-        System.out.println(token);
         DiscordClient client = DiscordClient.create(token);
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+        Mono<Void> login = client.withGateway(
+                (GatewayDiscordClient gateway) -> gateway.on(ReadyEvent.class, event -> Mono.fromRunnable(() -> {
+                    final User self = event.getSelf();
+                    System.out.printf("Logged in as %s", self.getTag());
+                })));
         login.block();
+    }
+
+    public void onLogin(DiscordClient client) {
+        // client.
     }
 }
